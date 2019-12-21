6a2d4bb9070c155f4487086bf92abb3f6f616c2f
Tochukwu Onwunali <onwunali91@yahoo.com>
2019-12-21 19:40:40 +0000

    Dashboard done

---
+++ public/app.js
@@ -0,0 +1,23 @@
+// const stat = document.querySelector(".sta");
+// const testP = document.querySelector(".pane1");
+// const testG = document.querySelector(".pane2");
+// const testC = document.querySelector(".pane3");
+// const testK = document.querySelector(".pane4");
+
+
+
+// const show = () => {
+//     testP.classList.toggle("panef")
+//     testC.classList.toggle("panef")
+//     testG.classList.toggle("panef")
+//     testK.classList.toggle("panef")
+//     console.log("statatatatatata")
+// }
+// stat.addEventListener("click",show)
+
+const form = document.querySelector("form");
+const form1 = form.querySelector(".input1").value
+const form2 = form.querySelector(".input2").value
+
+// const data = new FormData(form)
+console.log(form1)
